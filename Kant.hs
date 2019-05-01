7cb9a938ce2325e4341a69ab76fc7939b1a63c04
diekmann <none@localhost>
2019-05-01 20:24:57 +0200

    beispiel

--- Kant.hs
+++ Kant.hs
@@ -2,44 +2,45 @@ module Kant where
 
 import qualified Data.Set as S
 
-newtype Tatbestand = Tatbestand String
+newtype Tatbestand a = Tatbestand a
   deriving (Eq, Ord, Show)
-newtype Rechtsfolge = Rechtsfolge String
+newtype Rechtsfolge a = Rechtsfolge a
   deriving (Eq, Ord, Show)
 
-newtype Rechtsnorm = Rechtsnorm (Tatbestand, Rechtsfolge)
+data Rechtsnorm a b = Rechtsnorm (Tatbestand a) (Rechtsfolge b)
   deriving (Eq, Ord, Show)
 
 newtype Paragraph = Paragraph String
   deriving (Eq, Ord, Show)
 
-newtype Gesetz = Gesetz (S.Set (Paragraph, Rechtsnorm))
+newtype Gesetz a b = Gesetz (S.Set (Paragraph, Rechtsnorm a b))
   deriving (Eq, Ord, Show)
 
 -- https://de.wikipedia.org/wiki/Rechtsfolge
+beispiel_gesetz :: Gesetz String String
 beispiel_gesetz = Gesetz $ S.fromList [
   (Paragraph "§ 823 BGB",
    Rechtsnorm
-     (Tatbestand "Wer vorsätzlich oder fahrlässig das Leben, den Körper, die Gesundheit, (...), das Eigentum oder (...) eines anderen widerrechtlich verletzt,",
-      Rechtsfolge "ist dem anderen zum Ersatz des daraus entstehenden Schadens verpflichtet."
-     )
+     (Tatbestand "Wer vorsätzlich oder fahrlässig das Leben, den Körper, die Gesundheit, (...), das Eigentum oder (...) eines anderen widerrechtlich verletzt,")
+     (Rechtsfolge "ist dem anderen zum Ersatz des daraus entstehenden Schadens verpflichtet.")
   ),
   (Paragraph "§ 985 BGB",
    Rechtsnorm
-     (Tatbestand "Der Eigentümer einer Sache kann von dem Besitzer",
-      Rechtsfolge "die Herausgabe der Sache verlangen"
-     )
+     (Tatbestand "Der Eigentümer einer Sache kann von dem Besitzer")
+     (Rechtsfolge "die Herausgabe der Sache verlangen")
   ),
   (Paragraph "§ 303 StGB",
    Rechtsnorm
-     (Tatbestand "Wer rechtswidrig eine fremde Sache beschädigt oder zerstört,",
-      Rechtsfolge "wird mit Freiheitsstrafe bis zu zwei Jahren oder mit Geldstrafe bestraft."
-     )
+     (Tatbestand "Wer rechtswidrig eine fremde Sache beschädigt oder zerstört,")
+     (Rechtsfolge "wird mit Freiheitsstrafe bis zu zwei Jahren oder mit Geldstrafe bestraft.")
   )
   ]
 
 -- Beschreibt Handlungen als Änderung der Welt.
 newtype Handlung world = Handlung (world -> world)
+
+handeln :: world -> Handlung world -> world
+handeln welt (Handlung h) = h welt
 
 -- Beispiel, für eine Welt die nur aus einer Zahl besteht.
 -- Wenn die Zahl kleiner als 9000 ist erhöhe ich sie, ansonsten bleibt sie unverändert.
@@ -52,12 +53,30 @@ data Sollensanordnung = Gebot | Verbot | Erlaubnis | Freistellung
 -- Beschreibt ob eine Handlung in einer gegebenen Welt gut ist.
 -- Passt nicht so ganz auf die Definition von Maxime?
 newtype Maxime world = Maxime (world -> Handlung world -> Bool)
+--TODO: Maxime
 
 maxime_mir_ist_alles_recht = Maxime (\_ _ -> True)
 
 --TODO: Name passt nicht ganz
-verallgemeinern :: Maxime world -> S.Set Rechtsnorm
+verallgemeinern :: Maxime world -> S.Set (Rechtsnorm a b)
 verallgemeinern = undefined
+
+
+-- Wenn die welt nur eine Zahl ist, ...
+type Zahlengesetz = Gesetz (Integer, Integer) (Sollensanordnung)
+
+show_zahlengesetz :: Zahlengesetz -> String
+show_zahlengesetz (Gesetz g) = S.foldl (\s p-> s ++ show_paragraph p ++ "; ") "" g
+  where
+    show_paragraph (Paragraph p, rechtsnorm) = p ++ ": " ++ show_zahlenrechtsnorm rechtsnorm
+    show_zahlenrechtsnorm (Rechtsnorm (Tatbestand (a,b)) (Rechtsfolge f)) = "Wenn die welt " ++ show a ++ " ist und wir die welt nach " ++
+                                                                            show b ++ " aendern wollen, dann " ++ show f
+
+zahlengesetz_leer :: Zahlengesetz
+zahlengesetz_leer = Gesetz $ S.empty
+
+zahlengesetz_beispiel :: Zahlengesetz
+zahlengesetz_beispiel = Gesetz $ S.singleton ((Paragraph "Paragraph 42"), (Rechtsnorm (Tatbestand (1,2)) (Rechtsfolge Verbot)))
 
 -- Handle nur nach derjenigen Maxime, durch die du zugleich wollen kannst, dass sie ein allgemeines Gesetz werde.
 -- TODO unterstütze viele Maximen, wobei manche nicht zutreffen können?
@@ -65,10 +84,10 @@ kategorischer_imperativ ::
     world              -- Die Welt in ihrem aktuellen Zustand
     -> Handlung world  -- Eine mögliche Handlung, über die wir entscheiden wollen ob wir sie ausführen sollten.
     -> Maxime world    -- Persönliche Ethik?
-    -> Gesetz          -- Allgemeines Gesetz (für alle Menschen)
+    -> Gesetz a b      -- Allgemeines Gesetz (für alle Menschen)
     -- Ergebnis:
     -> (Sollensanordnung, -- Sollen wir die Handlung ausführen?
-        Gesetz)           -- Soll das allgemeine Gesetz entsprechend angepasst werden?
+        Gesetz a b)       -- Soll das allgemeine Gesetz entsprechend angepasst werden?
     --TODO: Wenn unsere Maximen perfekt und die Maximen aller Menschen konsisten sind, soll das Gesetz nur erweitert werden (append only)?
 kategorischer_imperativ welt handlung maxime gesetz =
     -- Es fehlt: ich muss nach allgemeinem Gesetz handeln. Wenn das Gesetz meinen Fall nicht abdeckt, dann muss meine Maxime zum Gesetz erhoben werden.
@@ -82,4 +101,4 @@ kategorischer_imperativ welt handlung maxime gesetz =
         --Nur ein Verbot wenn (bewerten handlung) für alle Menschen False ist.
         (Verbot, gesetz)
 
-beispiel_kategorischer_imperativ = kategorischer_imperativ 0 (Handlung (\n-> n+1)) maxime_mir_ist_alles_recht beispiel_gesetz
+beispiel_kategorischer_imperativ = kategorischer_imperativ 0 (Handlung (\n-> n+1)) maxime_mir_ist_alles_recht zahlengesetz_leer
