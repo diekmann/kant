6f96141f27a8c72ab2b138aedd16bc891c3f8a3c
diekmann <none@localhost>
2019-08-03 16:39:05 +0200

    improve example

--- Zahlenwelt.hs
+++ Zahlenwelt.hs
@@ -14,54 +14,56 @@ data Person = Alice | Bob | Carl
 
 -- Wenn die Welt sich durch eine Zahl darstellen lässt, ...
 data Zahlenwelt = Zahlenwelt {
-        verbleibend :: Integer, -- Ressourcen sind endlich. Verbleibende Ressourcen in der Welt.
-        besitz :: M.Map Person Integer -- Besitz jeder Person.
+        remaining :: Integer,            -- Resources are finite.
+        property :: M.Map Person Integer -- Estate of each person.
       }
   deriving (Eq, Ord, Show)
 
 abbauen :: Integer -> Person -> Zahlenwelt -> Zahlenwelt
-abbauen i p (Zahlenwelt verbleibend besitz) = Zahlenwelt (verbleibend-i) (M.adjust (+i) p besitz)
+abbauen i p (Zahlenwelt remaining property) = Zahlenwelt (remaining-i) (M.adjust (+i) p property)
 
 stehlen :: Integer -> Person -> Person -> Zahlenwelt -> Zahlenwelt
-stehlen _ opfer _ welt | M.notMember opfer (besitz welt) =
+stehlen _ opfer _ welt | M.notMember opfer (property welt) =
     welt -- Wenn das Opfer nichts hat kann auch nichts gestohlen werden.
-stehlen i opfer dieb (Zahlenwelt r besitz) = Zahlenwelt r neuer_besitz
-    where neuer_besitz = case M.lookup opfer besitz of
-                           Nothing -> besitz
-                           Just _ ->  M.insertWith (+) dieb i (M.adjust (\x -> x-i) opfer besitz)
+stehlen i opfer dieb (Zahlenwelt r property) = Zahlenwelt r neuer_property
+    where neuer_property = case M.lookup opfer property of
+                           Nothing -> property
+                           Just _ ->  M.insertWith (+) dieb i (M.adjust (\x -> x-i) opfer property)
 
 -- Eine Action ist nur physikalisch möglich, solange es noch Resourcen gibt.
 moeglich :: Person -> Zahlenwelt -> A.ActionF Person Zahlenwelt -> Bool
-moeglich person welt h = (verbleibend nach_handlung) >= 0
+moeglich person welt h = (remaining nach_handlung) >= 0
     where nach_handlung = A.after $ A.act person welt h
 
 -- Mehr ist mehr gut.
--- Globaler Fortschritt erlaubt stehlen, solange dabei nichts vernichtet wird.
-globaler_fortschritt :: A.Action Zahlenwelt -> Bool
--- Groesser (>) anstelle (>=) ist hier echt spannend! Es sagt, dass wir nicht act duerfen, wenn andere nicht die möglichkeit haben!!
-globaler_fortschritt (A.Action before after) = (gesamtbesitz after) >= (gesamtbesitz before) -- kein strenger Fortschritt, eher kein Rueckschritt
-    where gesamtbesitz w = M.foldl' (+) 0 (besitz w)
--- Dieser globale Fortschritt sollte eigentlich allgemeines Gesetz werden und die Maxime sollte individuelle Bereicherung sein (und die unsichtbare Hand macht den Rest. YOLO).
+global_progress :: A.Action Zahlenwelt -> Bool
+global_progress (A.Action before after) = (total after) >= (total before)
+    where total w = M.foldl' (+) 0 (property w)
+
+-- Allows e.g., stealing, as long as we don't destroy resources.
+--
+-- Interesting: use (>) instead of (>=)
 
 
-individueller_fortschritt :: Person -> A.Action Zahlenwelt -> Bool
-individueller_fortschritt p (A.Action before after) = (meins after) >= (meins before)
-    where meins welt = M.findWithDefault 0 p (besitz welt)
+individual_progress :: Person -> A.Action Zahlenwelt -> Bool
+individual_progress p (A.Action before after) = (mine after) >= (mine before)
+    where mine world = M.findWithDefault 0 p (property world)
 
 
--- TODO: Eigentlich wollen wir Fortschritt in ALLEN möglichen Welten.
-maxime_zahlenfortschritt = Debug.debug_maxime $ Kant.Maxime (\ich -> individueller_fortschritt ich)
--- Interessant: hard-coded Alice anstelle von 'ich'.
+maxime_zahlenfortschritt = Debug.debug_maxime $ Kant.Maxime (\me -> individual_progress me)
+
+-- Interesting: hard-code Alice instead of 'me'.
+
 
 zahlengesetz_beispiel :: Gesetze.CaseLaw Zahlenwelt
 zahlengesetz_beispiel = Gesetz $ S.singleton (
     (Paragraph 42),
-    (Rechtsnorm (Tatbestand (Zahlenwelt { verbleibend = 9000, besitz = M.singleton Alice 0 },
-                             Zahlenwelt { verbleibend = 9000, besitz = M.singleton Alice 1}))
+    (Rechtsnorm (Tatbestand (Zahlenwelt { remaining = 9000, property = M.singleton Alice 0 },
+                             Zahlenwelt { remaining = 9000, property = M.singleton Alice 1}))
                 (Rechtsfolge Verbot)))
 
 beispiel_kategorischer_imperativ = Kant.kategorischer_imperativ Alice
-    (Zahlenwelt { verbleibend = 9000, besitz = M.singleton Alice 0 })
+    (Zahlenwelt { remaining = 9000, property = M.singleton Alice 0 })
     (A.ActionF (abbauen 5))
     maxime_zahlenfortschritt
     Gesetze.case_law_ableiten
@@ -69,8 +71,8 @@ beispiel_kategorischer_imperativ = Kant.kategorischer_imperativ Alice
 
 
 delta_zahlenwelt :: Aenderung.Delta Zahlenwelt Person Integer
-delta_zahlenwelt before after = Aenderung.delta_num_map (besitz before) (besitz after)
-  --TODO wer braucht schon Natur und verbleibende Resourcen?
+delta_zahlenwelt before after = Aenderung.delta_num_map (property before) (property after)
+  --yolo: who needs nature and consider remaining resources?
 
 
 simulate :: (Ord a, Ord b) =>
@@ -98,10 +100,12 @@ simulate person maxime ableiten i h welt g =
     simulate person maxime ableiten (i-1) h w' g'
 
 initialwelt = Zahlenwelt {
-                verbleibend = 42,
-                besitz = M.fromList [(Alice, 5), (Bob, 10)]
+                remaining = 42,
+                property = M.fromList [(Alice, 5), (Bob, 10)]
               }
 
+-- Now we hard-code Alice as acting person in our examples (the maxim remains generic)
+ 
 beispiel_CaseLaw :: A.ActionF Person Zahlenwelt -> Gesetze.CaseLaw Zahlenwelt
 beispiel_CaseLaw h = simulate Alice maxime_zahlenfortschritt Gesetze.case_law_ableiten 10 h initialwelt leer
 
@@ -110,7 +114,6 @@ beispiel_CaseLawRelativ h = simulate Alice maxime_zahlenfortschritt (Gesetze.cas
 
 beispiel1 = beispiel_CaseLaw (A.ActionF (abbauen 5))
 beispiel1' = beispiel_CaseLawRelativ (A.ActionF (abbauen 5))
---putStrLn $ Gesetze.show_CaseLaw  beispiel1
 
 beispiel2 = beispiel_CaseLaw (A.ActionF (stehlen 5 Bob))
 beispiel2' = beispiel_CaseLawRelativ (A.ActionF (stehlen 5 Bob))
