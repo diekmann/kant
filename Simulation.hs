66fa136904466eb96221e84537a769098ee95545
diekmann <none@localhost>
2019-08-17 20:59:07 +0200

    In Teilfunktionen aufgeteilt.
    Mehr Typsignatur als Fukntion. Schreit das nach einer Library?

--- Simulation.hs
+++ Simulation.hs
@@ -22,6 +22,25 @@ data Optionen person world a b = Optionen {
 
 
 -- simulate one HandlungF
+simulateHandlungF :: (Ord a, Ord b) => (Enum person, Bounded person) => (Eq world) =>
+  Optionen person world a b
+  -> H.HandlungF person world       -- Beabsichtigte Handlung
+  -> world                          -- Initialwelt.
+  -> G.Gesetz Integer a b             -- Initialgesetz
+  -> (world, G.Gesetz Integer a b)
+simulateHandlungF so h welt g | not ((moeglich so) (H.handeln (person so) welt h)) = (welt, g)
+simulateHandlungF so h welt g =
+  let (sollensanordnung, g') = Kant.kategorischer_imperativ (person so) welt h (maxime so ) (allgemeines_gesetz_ableiten so) g in
+  let w' = (if sollensanordnung == G.Erlaubnis && ((moeglich so) (H.handeln (person so) welt h))
+            then
+              H.nachher (H.handeln (person so) welt h)
+            else
+              welt
+           ) in
+  (w', g')
+
+
+-- simulate one HandlungF
 simulateOne :: (Ord a, Ord b) => (Enum person, Bounded person) => (Eq world) =>
   Optionen person world a b
   -> Int                            -- maximale Anzahl Iterationen (Simulationen)
@@ -30,15 +49,8 @@ simulateOne :: (Ord a, Ord b) => (Enum person, Bounded person) => (Eq world) =>
   -> G.Gesetz Integer a b             -- Initialgesetz
   -> G.Gesetz Integer a b
 simulateOne _  i _ _    g | i <= 0 = g -- iteration vorbei
-simulateOne so _ h welt g | not ((moeglich so) (H.handeln (person so) welt h)) = g
 simulateOne so i h welt g =
-  let (sollensanordnung, g') = Kant.kategorischer_imperativ (person so) welt h (maxime so ) (allgemeines_gesetz_ableiten so) g in
-  let w' = (if sollensanordnung == G.Erlaubnis && ((moeglich so) (H.handeln (person so) welt h))
-            then
-              H.nachher (H.handeln (person so) welt h)
-            else
-              welt
-           ) in
+  let (w', g') = simulateHandlungF so h welt g in
   if welt == w' then
     g'
   else
@@ -50,7 +62,7 @@ gesetzbuch_generator :: (Ord a, Ord b) => (Enum person, Bounded person) => (Eq w
   Optionen person world a b
   -> world
   -> Gen (G.Gesetz Integer a b)
--- liste mit Historie was passiert ist und zu welchem Gesetz fuehrte.
+-- TODO: liste mit Historie was passiert ist und zu welchem Gesetz fuehrte.
 gesetzbuch_generator so welt = do
     h <- arbitrary
     return $ simulateOne so 20 h welt G.leer
