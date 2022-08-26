74f04422fa24f4db17145b4b40d17b3949905feb
diekmann <nobody>
2022-08-26 16:00:58 +0200

    Beispiel!

--- Formal/Kant.thy
+++ Formal/Kant.thy
@@ -81,15 +81,34 @@ lemma bevoelkerung_example_very_finite_population [code_unfold]:
   unfolding bevoelkerung_def
   by(auto intro:example_very_finite_population.exhaust UNIV_eq_I)
 
-lemma [code_unfold]: "teste_maxime welt handlung maxime =
+lemma example_teste_maxime_exhaust [code_unfold]: "teste_maxime welt handlung maxime =
         teste_maxime_exhaust [Alice, Bob, Carol, Eve] welt handlung maxime"
   apply(rule teste_maxime_exhaust)
   by(simp add: bevoelkerung_example_very_finite_population[simplified bevoelkerung_def])
 
 declare teste_maxime_def[code del] \<comment>\<open>Only use executable \<^const>\<open>teste_maxime_exhaust\<close>\<close>
 
-value \<open>teste_maxime
+text\<open>Beispiel: Die Mir-ist-alles-Recht Maxime ist erfuellt.\<close>
+lemma \<open>teste_maxime
             (42::nat)
             (HandlungF (\<lambda>(person::example_very_finite_population) welt. welt + 1))
-            (Maxime (\<lambda>_ _. True))\<close>
+            (Maxime (\<lambda>_ _. True))\<close> by eval
+
+text\<open>Beispiel:
+Die Welt ist modelliert als eine Abbildung von Person auf Besitz.
+Die Maxime sagt, dass Leute immer mehr oder gleich viel wollen, aber nie etwas verlieren wollen.
+In einer Welt in der keiner etwas hat erfuellt die Handlung jemanden 3 zu geben die Maxime.
+\<close>
+lemma \<open>teste_maxime
+            [Alice \<mapsto> (0::nat), Bob \<mapsto> 0, Carol \<mapsto> 0, Eve \<mapsto> 0]
+            (HandlungF (\<lambda>person welt. welt(person \<mapsto> 3)))
+            (Maxime (\<lambda>person handlung. (the ((vorher handlung) person)) \<le> (the ((nachher handlung) person))))\<close>
+  by eval
+text\<open>Wenn nun \<^const>\<open>Bob\<close> allerdings bereits 4 hat wuerde die obige Handlung ein Verlust
+fuer ihn bedeuten und die Maxime ist nicht erfuellt.\<close>
+lemma \<open>\<not> teste_maxime
+            [Alice \<mapsto> (0::nat), Bob \<mapsto> 4, Carol \<mapsto> 0, Eve \<mapsto> 0]
+            (HandlungF (\<lambda>person welt. welt(person \<mapsto> 3)))
+            (Maxime (\<lambda>person handlung. (the ((vorher handlung) person)) \<le> (the ((nachher handlung) person))))\<close>
+  by eval
 end
