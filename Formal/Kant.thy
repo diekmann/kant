9f02242d37d8a4eb439a6d7982687a48785d6c8f
diekmann <nobody>
2022-08-26 13:08:36 +0200

    trying to get stuff executable

--- Formal/Kant.thy
+++ Formal/Kant.thy
@@ -49,7 +49,52 @@ definition teste_maxime ::
 "teste_maxime welt handlung maxime \<equiv>
   \<forall>p \<in> bevoelkerung. was_wenn_jeder_so_handelt_aus_sicht_von welt handlung maxime p"
 
-lemma "teste_maxime welt handlung (Maxime m) =
+lemma teste_maxime_unfold:
+  "teste_maxime welt handlung (Maxime m) =
         (\<forall>p\<in>bevoelkerung. \<forall>x\<in>bevoelkerung. m p (handeln x welt handlung))"
   by(simp add: teste_maxime_def wenn_jeder_so_handelt_def)
+lemma teste_maxime_crossproduct_unfold: (*WARNING: rhs not fully simp'ed!*)
+  "teste_maxime welt handlung (Maxime m) =
+        (\<forall>(p,x)\<in>bevoelkerung\<times>bevoelkerung. m p (handeln x welt handlung))"
+  unfolding teste_maxime_unfold by simp
+
+definition teste_maxime_exhaust where
+  "teste_maxime_exhaust bevoelk welt handlung maxime \<equiv>
+    (case maxime of (Maxime m) \<Rightarrow> 
+      list_all (\<lambda>(p,x). m p (handeln x welt handlung)) (List.product bevoelk bevoelk))"
+
+lemma teste_maxime_exhaust: "set b = (UNIV::'person set) \<Longrightarrow>
+        teste_maxime welt handlung maxime = teste_maxime_exhaust b welt handlung maxime"
+  apply(case_tac maxime, rename_tac m, simp)
+  unfolding teste_maxime_crossproduct_unfold teste_maxime_exhaust_def bevoelkerung_def
+  apply(simp)
+  by(simp add: list_all_iff)
+  
+  
+
+datatype example_very_finite_population = Alice | Bob
+(*lemma UNIV_example_very_finite_world [code_unfold]:
+  "UNIV = {AliceHappy, AliceSad, BobHappy, BobSad}"
+  by(auto intro:example_very_finite_world.exhaust UNIV_eq_I)
+lemma coset_example_very_finite_world [code_unfold]:
+  "List.coset [] = {AliceHappy, AliceSad, BobHappy, BobSad}"
+  using UNIV_example_very_finite_world by simp*)
+lemma bevoelkerung_example_very_finite_population [code_unfold]:
+  "bevoelkerung = {Alice, Bob}"
+  unfolding bevoelkerung_def
+  by(auto intro:example_very_finite_population.exhaust UNIV_eq_I)
+
+declare UNIV_coset[code del]
+declare bevoelkerung_def[code del]
+code_thms teste_maxime
+
+(*does not replace bevoekerung with the set for this type*)
+value[nbe] \<open>teste_maxime
+            (42::nat)
+            (HandlungF (\<lambda>(person::example_very_finite_population) welt. welt + 1))
+            (Maxime (\<lambda>_ _. True))\<close>
+value \<open>teste_maxime_exhaust [Alice, Bob]
+            (42::nat)
+            (HandlungF (\<lambda>(person::example_very_finite_population) welt. welt + 1))
+            (Maxime (\<lambda>_ _. True))\<close>
 end
