a25f5f80de5703a9b0c0820ad89f98cf325fba62
diekmann <nobody>
2022-09-08 23:34:38 +0200

    Steuern vereinfacht

--- Formal/Steuern.thy
+++ Formal/Steuern.thy
@@ -8,19 +8,15 @@ text\<open>Basierend auf einer stark vereinfachten Version des deutschen Steuerr
 Wenn ich Wikipedia richtig verstanden habe, habe ich sogar aus Versehen einen Teil des 
 österreichischen Steuersystem gebaut mit deutschen Konstanten.\<close>
 
-datatype Person = Person (einkommen: nat)
-
-value "einkommen (Person 4)"
 
 locale steuersystem =
-  fixes steuer :: "Person \<Rightarrow> nat"
+  fixes steuer :: "nat \<Rightarrow> nat" \<comment>\<open>Einkommen -> Steuer\<close>
   
-  assumes monotonie: "einkommen a \<ge> einkommen b \<Longrightarrow> steuer a \<ge> steuer b"
+  assumes wer_hat_der_gibt:
+    "einkommen_a \<ge> einkommen_b \<Longrightarrow> steuer einkommen_a \<ge> steuer einkommen_b"
 begin
-  definition brutto :: "Person \<Rightarrow> nat" where
-    "brutto p \<equiv> einkommen p"
-  definition netto :: "Person \<Rightarrow> nat" where
-    "netto p \<equiv> (brutto p) - (steuer p)"
+  definition netto :: "nat \<Rightarrow> nat" where
+    "netto einkommen \<equiv> einkommen - (steuer einkommen)"
 
   (*TODO: mehr einkommen \<ge> mehr netto*)
   (*TODO: mehr einkommen \<Rightarrow> hoeherer Steuersatz*)
@@ -42,6 +38,18 @@ lemma zonensteuermono: "e1 \<le> e2
    apply(simp; fail)
   by(simp add: mult_right_mono percentage_range)
 
+text\<open>Kein Einkommen -> keine Steuer\<close>
+lemma zonensteuer_zero: "zonensteuer ls p 0 = 0"
+  by(induction ls) auto
+
+text\<open>Steuer ist immer positiv.\<close>
+  (*Da das Einkommen ein nat ist, ist das Einkommen hier auch immer positiv.
+    Eventuell will ich aber mal das Einkommen auf int aendern,
+    dann muss das folgende Lemma aber immernoch gelten!*)
+lemma zonensteuer_pos: "zonensteuer ls p e \<ge> 0"
+  apply(induction ls)
+   apply(simp add: percentage_range)
+  by (metis zero_le zonensteuer_zero zonensteuermono)
 
 definition steuerzonen2022 :: "(nat \<times> percentage) list" where
   "steuerzonen2022 \<equiv> [
@@ -51,7 +59,7 @@ definition steuerzonen2022 :: "(nat \<times> percentage) list" where
                        (219229, percentage 0.42)
                       ]"
 
-fun steuerzonenAbs :: "(nat \<times> percentage ) list \<Rightarrow> (nat \<times> percentage ) list" where
+fun steuerzonenAbs :: "(nat \<times> percentage) list \<Rightarrow> (nat \<times> percentage) list" where
    "steuerzonenAbs [] = []"
  |  "steuerzonenAbs ((zone, prozent)#zonen) = 
       (zone,prozent)#(map (\<lambda>(z,p). (zone+z, p)) (steuerzonenAbs zonen))"
@@ -130,20 +138,22 @@ lemma floorD: "a \<le> b \<Longrightarrow> floor a \<le> floor b"
   apply(simp add: floor_def)
   by linarith
 
-definition einkommenssteuer :: "Person \<Rightarrow> nat" where
-  "einkommenssteuer p \<equiv>
-    floor (bucketsteuerAbs steuerbuckets2022 (percentage  0.45) (einkommen p))"
+definition einkommenssteuer :: "nat \<Rightarrow> nat" where
+  "einkommenssteuer einkommen \<equiv>
+    floor (bucketsteuerAbs steuerbuckets2022 (percentage  0.45) einkommen)"
 
-lemma \<open>einkommenssteuer (Person 10) = 0\<close> by eval
-lemma \<open>einkommenssteuer (Person 10000) = 0\<close> by eval
-lemma \<open>einkommenssteuer (Person 14000) = floor ((14000-10347)*0.14)\<close> by eval
-lemma \<open>einkommenssteuer (Person 20000) =
+value \<open>einkommenssteuer 10\<close>
+lemma \<open>einkommenssteuer 10 = 0\<close> by eval
+lemma \<open>einkommenssteuer 10000 = 0\<close> by eval
+lemma \<open>einkommenssteuer 14000 = floor ((14000-10347)*0.14)\<close> by eval
+lemma \<open>einkommenssteuer 20000 =
         floor ((14926-10347)*0.14 + (20000-14926)*0.2397)\<close> by eval
-value \<open>einkommenssteuer (Person 40000)\<close>
-value \<open>einkommenssteuer (Person 60000)\<close>
+value \<open>einkommenssteuer 40000\<close>
+value \<open>einkommenssteuer 60000\<close>
 
 lemma einkommenssteuer:
-  "einkommenssteuer p = floor (zonensteuer steuerzonen2022 (percentage 0.45) (einkommen p))"
+  "einkommenssteuer einkommen =
+    floor (zonensteuer steuerzonen2022 (percentage 0.45) einkommen)"
   apply(simp add: einkommenssteuer_def)
   apply(simp add: steuerbuckets2022)
   apply(subst bucketsteuerAbs_zonensteuer)
@@ -154,8 +164,9 @@ lemma einkommenssteuer:
 interpretation steuersystem
   where steuer = einkommenssteuer
 proof
-  fix a :: Person and b :: Person
-  show "einkommen a \<le> einkommen b \<Longrightarrow> einkommenssteuer a \<le> einkommenssteuer b"
+  fix einkommen_a and einkommen_b
+  show "einkommen_a \<le> einkommen_b
+        \<Longrightarrow> einkommenssteuer einkommen_a \<le> einkommenssteuer einkommen_b"
     apply(simp add: einkommenssteuer)
     apply(rule floorD)
     apply(rule zonensteuermono)
