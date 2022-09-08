774c2a4c23692093d0e220d09e10470f49902a93
diekmann <nobody>
2022-09-08 23:55:02 +0200

    steuern

--- Formal/BeispielSteuern.thy
+++ Formal/BeispielSteuern.thy
@@ -1,5 +1,5 @@
 theory BeispielSteuern
-imports Kant Gesetze Simulation
+imports Kant Gesetze Simulation Steuern
 begin
 
 
@@ -56,7 +56,7 @@ definition "sc' \<equiv> SimConsts
 
 definition "initialwelt \<equiv> Steuerwelt (KE(Alice:=8, Bob:=3, Eve:= 5))"
 
-definition "beispiel_case_law h \<equiv> simulateOne sc 20 h initialwelt (Gesetz {})"
+definition "beispiel_case_law h \<equiv> simulateOne sc 3 h initialwelt (Gesetz {})"
 definition "beispiel_case_law' h \<equiv> simulateOne sc' 20 h initialwelt (Gesetz {})"
 
 text\<open>Keiner zahlt steuern: funktioniert\<close>
@@ -83,115 +83,14 @@ lemma \<open>beispiel_case_law'
     ))) =
   Gesetz {(Paragraph 1, Rechtsnorm (Tatbestand [Verliert Alice 1]) (Rechtsfolge Verbot))}\<close> by eval
   
-text\<open>Jeder muss steuern zahlen: funktioniert, ist aber doof, denn am Ende sind alle im Minus.\<close>
+text\<open>Jeder muss steuern zahlen:
+  funktioniert, ist aber doof, denn am Ende sind alle im Minus.
+
+Das \<^term>\<open>ich\<close> wird garnicht verwendet, da jeder Steuern zahlt.\<close>
 lemma \<open>beispiel_case_law
-  (HandlungF (\<lambda>ich welt. Steuerwelt (
-        ((\<lambda>e. e - 1) \<circ> (get_einkommen welt))
-    ))) =
+  (HandlungF (\<lambda>ich welt. Steuerwelt ((\<lambda>e. e - 1) \<circ> (get_einkommen welt)))) =
 Gesetz
-  {(Paragraph 20,
-    Rechtsnorm
-     (Tatbestand
-       ([(Alice, - 11), (Bob, - 16), (Carol, - 19), (Eve, - 14)],
-        [(Alice, - 12), (Bob, - 17), (Carol, - 20), (Eve, - 15)]))
-     (Rechtsfolge Erlaubnis)),
-   (Paragraph 19,
-    Rechtsnorm
-     (Tatbestand
-       ([(Alice, - 10), (Bob, - 15), (Carol, - 18), (Eve, - 13)],
-        [(Alice, - 11), (Bob, - 16), (Carol, - 19), (Eve, - 14)]))
-     (Rechtsfolge Erlaubnis)),
-   (Paragraph 18,
-    Rechtsnorm
-     (Tatbestand
-       ([(Alice, - 9), (Bob, - 14), (Carol, - 17), (Eve, - 12)],
-        [(Alice, - 10), (Bob, - 15), (Carol, - 18), (Eve, - 13)]))
-     (Rechtsfolge Erlaubnis)),
-   (Paragraph 17,
-    Rechtsnorm
-     (Tatbestand
-       ([(Alice, - 8), (Bob, - 13), (Carol, - 16), (Eve, - 11)],
-        [(Alice, - 9), (Bob, - 14), (Carol, - 17), (Eve, - 12)]))
-     (Rechtsfolge Erlaubnis)),
-   (Paragraph 16,
-    Rechtsnorm
-     (Tatbestand
-       ([(Alice, - 7), (Bob, - 12), (Carol, - 15), (Eve, - 10)],
-        [(Alice, - 8), (Bob, - 13), (Carol, - 16), (Eve, - 11)]))
-     (Rechtsfolge Erlaubnis)),
-   (Paragraph 15,
-    Rechtsnorm
-     (Tatbestand
-       ([(Alice, - 6), (Bob, - 11), (Carol, - 14), (Eve, - 9)],
-        [(Alice, - 7), (Bob, - 12), (Carol, - 15), (Eve, - 10)]))
-     (Rechtsfolge Erlaubnis)),
-   (Paragraph 14,
-    Rechtsnorm
-     (Tatbestand
-       ([(Alice, - 5), (Bob, - 10), (Carol, - 13), (Eve, - 8)],
-        [(Alice, - 6), (Bob, - 11), (Carol, - 14), (Eve, - 9)]))
-     (Rechtsfolge Erlaubnis)),
-   (Paragraph 13,
-    Rechtsnorm
-     (Tatbestand
-       ([(Alice, - 4), (Bob, - 9), (Carol, - 12), (Eve, - 7)],
-        [(Alice, - 5), (Bob, - 10), (Carol, - 13), (Eve, - 8)]))
-     (Rechtsfolge Erlaubnis)),
-   (Paragraph 12,
-    Rechtsnorm
-     (Tatbestand
-       ([(Alice, - 3), (Bob, - 8), (Carol, - 11), (Eve, - 6)],
-        [(Alice, - 4), (Bob, - 9), (Carol, - 12), (Eve, - 7)]))
-     (Rechtsfolge Erlaubnis)),
-   (Paragraph 11,
-    Rechtsnorm
-     (Tatbestand
-       ([(Alice, - 2), (Bob, - 7), (Carol, - 10), (Eve, - 5)],
-        [(Alice, - 3), (Bob, - 8), (Carol, - 11), (Eve, - 6)]))
-     (Rechtsfolge Erlaubnis)),
-   (Paragraph 10,
-    Rechtsnorm
-     (Tatbestand
-       ([(Alice, - 1), (Bob, - 6), (Carol, - 9), (Eve, - 4)],
-        [(Alice, - 2), (Bob, - 7), (Carol, - 10), (Eve, - 5)]))
-     (Rechtsfolge Erlaubnis)),
-   (Paragraph 9,
-    Rechtsnorm
-     (Tatbestand
-       ([(Alice, 0), (Bob, - 5), (Carol, - 8), (Eve, - 3)],
-        [(Alice, - 1), (Bob, - 6), (Carol, - 9), (Eve, - 4)]))
-     (Rechtsfolge Erlaubnis)),
-   (Paragraph 8,
-    Rechtsnorm
-     (Tatbestand
-       ([(Alice, 1), (Bob, - 4), (Carol, - 7), (Eve, - 2)],
-        [(Alice, 0), (Bob, - 5), (Carol, - 8), (Eve, - 3)]))
-     (Rechtsfolge Erlaubnis)),
-   (Paragraph 7,
-    Rechtsnorm
-     (Tatbestand
-       ([(Alice, 2), (Bob, - 3), (Carol, - 6), (Eve, - 1)],
-        [(Alice, 1), (Bob, - 4), (Carol, - 7), (Eve, - 2)]))
-     (Rechtsfolge Erlaubnis)),
-   (Paragraph 6,
-    Rechtsnorm
-     (Tatbestand
-       ([(Alice, 3), (Bob, - 2), (Carol, - 5), (Eve, 0)],
-        [(Alice, 2), (Bob, - 3), (Carol, - 6), (Eve, - 1)]))
-     (Rechtsfolge Erlaubnis)),
-   (Paragraph 5,
-    Rechtsnorm
-     (Tatbestand
-       ([(Alice, 4), (Bob, - 1), (Carol, - 4), (Eve, 1)],
-        [(Alice, 3), (Bob, - 2), (Carol, - 5), (Eve, 0)]))
-     (Rechtsfolge Erlaubnis)),
-   (Paragraph 4,
-    Rechtsnorm
-     (Tatbestand
-       ([(Alice, 5), (Bob, 0), (Carol, - 3), (Eve, 2)],
-        [(Alice, 4), (Bob, - 1), (Carol, - 4), (Eve, 1)]))
-     (Rechtsfolge Erlaubnis)),
-   (Paragraph 3,
+  {(Paragraph 3,
     Rechtsnorm
      (Tatbestand
        ([(Alice, 6), (Bob, 1), (Carol, - 2), (Eve, 3)],
@@ -210,11 +109,38 @@ Gesetz
         [(Alice, 7), (Bob, 2), (Carol, - 1), (Eve, 4)]))
      (Rechtsfolge Erlaubnis))}\<close> by eval
 lemma \<open>beispiel_case_law'
-  (HandlungF (\<lambda>ich welt. Steuerwelt (
-        ((\<lambda>e. e - 1) \<circ> (get_einkommen welt))
-    ))) =
+  (HandlungF (\<lambda>ich welt. Steuerwelt ((\<lambda>e. e - 1) \<circ> (get_einkommen welt)))) =
   Gesetz
   {(Paragraph 1,
     Rechtsnorm (Tatbestand [Verliert Alice 1, Verliert Bob 1, Verliert Carol 1, Verliert Eve 1])
+     (Rechtsfolge Erlaubnis))}\<close> by eval
+
+text\<open>Jetzt kommt die Steuern.thy ins Spiel.\<close>
+(*wow ist das langsam!*)
+
+text\<open>Bei dem geringen Einkommen zahlt keiner Steuern.\<close>
+lemma \<open>beispiel_case_law
+  (HandlungF (\<lambda>ich welt. Steuerwelt (
+        ((\<lambda>e. e - einkommenssteuer e) \<circ> nat \<circ> (get_einkommen welt))
+    ))) = 
+  Gesetz
+  {(Paragraph 1,
+    Rechtsnorm
+     (Tatbestand
+       ([(Alice, 8), (Bob, 3), (Carol, 0), (Eve, 5)],
+        [(Alice, 8), (Bob, 3), (Carol, 0), (Eve, 5)]))
+     (Rechtsfolge Erlaubnis))}\<close> by eval
+
+lemma \<open>simulateOne
+  sc' 1
+  (HandlungF (\<lambda>ich welt. Steuerwelt (
+        ((\<lambda>e. e - einkommenssteuer e) \<circ> nat \<circ> (get_einkommen welt))
+    )))
+  (Steuerwelt (KE(Alice:=10000, Bob:=14000, Eve:= 20000)))
+  (Gesetz {})
+  =
+  Gesetz
+  {(Paragraph 1,
+    Rechtsnorm (Tatbestand [Verliert Bob 511, Verliert Eve 1857])
      (Rechtsfolge Erlaubnis))}\<close> by eval
 end
