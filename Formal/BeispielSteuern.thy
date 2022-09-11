91a46d608bb1a54f858924afb721cc45157ab6c8
diekmann <nobody>
2022-09-11 20:53:11 +0200

    steuersystem und maxime sind vereinbar

--- Formal/BeispielSteuern.thy
+++ Formal/BeispielSteuern.thy
@@ -34,15 +34,13 @@ lemma \<open>mehrverdiener Alice
         (Handlung (Steuerwelt (KE(Alice:=8, Bob:=12, Eve:=7))) (Steuerwelt (KE(Alice:=5))))
        = {Alice, Bob}\<close> by eval
 
-(*TODO: maxime sollte sein, dass ich mehr steuern zu zahlen hab als geringerverdiener.*)
+(*TODO: eine andere test maxime sollte sein, dass ich mehr steuern zu zahlen hab als geringerverdiener.*)
 definition maxime_steuern :: "(person, steuerwelt) maxime" where
   "maxime_steuern \<equiv> Maxime 
       (\<lambda>ich handlung.
            \<forall>p\<in>mehrverdiener ich handlung.
                 steuerlast ich handlung \<le> steuerlast p handlung)"
 
-
-(*kann ich delta als class implementieren?*)
 
 fun delta_steuerwelt :: "(steuerwelt, person, int) delta" where
   "delta_steuerwelt (Handlung vor nach) =
@@ -121,8 +119,10 @@ text\<open>Jetzt kommt die Steuern.thy ins Spiel.\<close>
 (*wow ist das langsam!*)
 
 text\<open>Bei dem geringen Einkommen zahlt keiner Steuern.\<close>
-definition "jeder_zahlt_einkommenssteuer ich welt \<equiv>
-  Steuerwelt ((\<lambda>e. e - einkommenssteuer e) \<circ> nat \<circ> (get_einkommen welt))"
+definition "jeder_zahlt steuerberechnung ich welt \<equiv>
+  Steuerwelt ((\<lambda>e. e - steuerberechnung e) \<circ> nat \<circ> (get_einkommen welt))"
+
+definition "jeder_zahlt_einkommenssteuer \<equiv> jeder_zahlt einkommenssteuer"
 lemma \<open>beispiel_case_law (HandlungF jeder_zahlt_einkommenssteuer ) = 
   Gesetz
   {(Paragraph 1,
@@ -142,4 +142,22 @@ lemma \<open>simulateOne
   {(Paragraph 1,
     Rechtsnorm (Tatbestand [Verliert Bob 511, Verliert Eve 1857])
      (Rechtsfolge Erlaubnis))}\<close> by eval
+
+(*TODO: eigentlich sollte das folgende gelten in beide richtungen:*)
+text\<open>Die Anforderungen fuer ein \<^locale>\<open>steuersystem\<close> und die \<^const>\<open>maxime_steuern\<close> sind vereinbar.\<close>
+lemma "steuersystem steuersystem_impl \<Longrightarrow>
+        (\<forall>welt. teste_maxime welt (HandlungF (jeder_zahlt steuersystem_impl)) maxime_steuern)"
+  (*apply(rule iffI)*)
+   apply(simp add: maxime_steuern_def teste_maxime_unfold)
+   apply(simp add: jeder_zahlt_def bevoelkerung_def)
+   apply(intro allI impI)
+  apply(rename_tac welt p1 p2)
+  thm steuersystem.wer_hat_der_gibt
+   apply(drule_tac
+        einkommen_b="nat (get_einkommen welt p1)" and
+        einkommen_a="nat (get_einkommen welt p2)" in steuersystem.wer_hat_der_gibt)
+    apply(simp; fail)
+   apply(simp; fail)
+  done
+
 end
