1f148eb75a8a3444b95a139ff6f1cb218a55262f
diekmann <nobody>
2022-09-24 10:08:46 +0200

    ...

--- Formal/BeispielZahlenwelt.thy
+++ Formal/BeispielZahlenwelt.thy
@@ -7,7 +7,7 @@ section\<open>Beispiel: Zahlenwelt\<close>
 text\<open>Wir nehmen an, die Welt lässt sich durch eine Zahl darstellen,
 die den Besitz einer Person modelliert.\<close>
 datatype zahlenwelt = Zahlenwelt
-        "person \<Rightarrow> int option" \<comment> \<open>besitz: Besitz jeder Person.\<close>
+  "person \<Rightarrow> int option \<comment> \<open>besitz: Besitz jeder Person.\<close>"
 
 fun gesamtbesitz :: "zahlenwelt \<Rightarrow> int" where
   "gesamtbesitz (Zahlenwelt besitz) = sum_list (List.map_filter besitz Enum.enum)"
@@ -28,6 +28,8 @@ fun delta_zahlenwelt :: "(zahlenwelt, person, int) delta" where
       Aenderung.delta_num_map (Handlung vor_besitz nach_besitz)"
 (*>*)
 
+subsection\<open>Handlungen\<close>
+
 text\<open>Die folgende Handlung erschafft neuen Besitz aus dem Nichts:\<close>
 fun erschaffen :: "nat \<Rightarrow> person \<Rightarrow> zahlenwelt \<Rightarrow> zahlenwelt" where
   "erschaffen i p (Zahlenwelt besitz) =
@@ -36,13 +38,25 @@ fun erschaffen :: "nat \<Rightarrow> person \<Rightarrow> zahlenwelt \<Rightarro
         of None \<Rightarrow> besitz(p \<mapsto> int i)
          | Some b \<Rightarrow> besitz(p \<mapsto> b + int i))"
 
-text\<open>Wir definieren eine Maxime die besagt,
-dass sich der Besitz einer Person nicht verringern darf:\<close>
-fun individueller_fortschritt :: "person \<Rightarrow> zahlenwelt handlung \<Rightarrow> bool" where
-  "individueller_fortschritt p (Handlung vor nach) \<longleftrightarrow> (meins p vor) \<le> (meins p nach)"
-definition maxime_zahlenfortschritt :: "(person, zahlenwelt) maxime" where
-  "maxime_zahlenfortschritt \<equiv> Maxime (\<lambda>ich. individueller_fortschritt ich)"
+(*
+fun stehlen :: "int \<Rightarrow> person \<Rightarrow> person \<Rightarrow> zahlenwelt \<Rightarrow> zahlenwelt"
+  where
+"stehlen i opfer dieb (Zahlenwelt besitz) = 
+  (case besitz opfer
+     of None \<Rightarrow> Zahlenwelt besitz \<comment>\<open>Wenn das Opfer nichts hat kann auch nichts gestohlen werden.\<close>
+      | Some beute \<Rightarrow> Zahlenwelt besitz(dieb \<mapsto> += beute)
+  )"
 
+value \<open>\<close>
+stehlen _ opfer _ welt | M.notMember opfer (besitz welt) =
+    welt -- 
+stehlen i opfer dieb (Zahlenwelt r besitz) = Zahlenwelt r neuer_besitz
+    where neuer_besitz = case M.lookup opfer besitz of
+                           Nothing -> besitz
+                           Just _ ->  M.insertWith (+) dieb i (M.adjust (\x -> x-i) opfer besitz)
+*)
+
+subsection\<open>Setup\<close>
 
 definition "initialwelt \<equiv> Zahlenwelt [Alice \<mapsto> 5, Bob \<mapsto> 10]"
 
@@ -64,6 +78,15 @@ definition "beispiel_case_law_relativ maxime handlung \<equiv>
     20 handlung initialwelt (Gesetz {})"
 
 subsection\<open>Alice erzeugt 5 Wohlstand für sich.\<close>
+
+text\<open>Wir definieren eine Maxime die besagt,
+dass sich der Besitz einer Person nicht verringern darf:\<close>
+fun individueller_fortschritt :: "person \<Rightarrow> zahlenwelt handlung \<Rightarrow> bool" where
+  "individueller_fortschritt p (Handlung vor nach) \<longleftrightarrow> (meins p vor) \<le> (meins p nach)"
+definition maxime_zahlenfortschritt :: "(person, zahlenwelt) maxime" where
+  "maxime_zahlenfortschritt \<equiv> Maxime (\<lambda>ich. individueller_fortschritt ich)"
+
+
 
 text\<open>Alice kann beliebig oft 5 Wohlstand für sich selbst erschaffen.
 Das entstehende Gesetz ist nicht sehr gut, da es einfach jedes Mal einen
