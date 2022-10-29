c7a4fddab0be92626a7a6d8909ad23292ebdd377
Lars Hupel <lars.hupel@mytum.de>
2022-10-29 20:41:05 +0200

    delete unneeded Cache.thy

--- Formal/Cache/Cache.thy
+++
@@ -1,7 +0,0 @@
-theory Cache
-  imports "HOL-Library.Extended_Real" "HOL-Library.Code_Target_Numeral"
-begin
-
-text\<open>The only purpose of this theory is to pre-load theories and cache them in order to speed up builds.\<close>
-
-end
