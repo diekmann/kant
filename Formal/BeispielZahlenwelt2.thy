59b593a065590da7ec721a2767c41424225bd77d
nobody <nobody>
2022-12-08 16:28:02 +0100

    ein oops weniger

--- Formal/BeispielZahlenwelt2.thy
+++ Formal/BeispielZahlenwelt2.thy
@@ -359,8 +359,7 @@ lemma \<open>\<forall>h \<in> set (alle_moeglichen_handlungen initialwelt [Handl
 
 
 lemma \<open>wohlgeformte_maxime zahlenwps maxime_hatte_konsens\<close>
-  apply(simp add: wohlgeformte_maxime_def wohlgeformte_maxime_auf_def maxime_hatte_konsens_def)
-  oops
+  by(simp add: wohlgeformte_maxime_def wohlgeformte_maxime_auf_def maxime_hatte_konsens_def hat_konsens_swap)
 
 lemma \<open>erzeuge_beispiel
   zahlenwps initialwelt
