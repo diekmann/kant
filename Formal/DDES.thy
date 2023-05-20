9883bd24e80e34a8cffef300eca173321d9a6592
diekmann <nobody>
2023-05-20 13:04:08 +0200

    factor out event evaluation

--- Formal/DDES.thy
+++ Formal/DDES.thy
@@ -8,67 +8,96 @@ type_synonym time = nat (*months.
 Because months seem to be the only time units which allow to consistently split a year
 into something more fine-grained without having to worry about leap yeas.*)
 
-datatype 'world event =
+text\<open>Raw events will be of type \<^typ>\<open>'event\<close>.
+We want them as a datatype (instead of a function) to reason about them.
+An executor must describe how an event is actually executed,
+by describing how it modifies the world.\<close>
+type_synonym ('world, 'event) executor = \<open>'world \<Rightarrow> 'event \<Rightarrow> 'world\<close>
+
+
+text\<open>Events for the Simulator.\<close>
+datatype 'event timed_event =
   SingletonEvent
-    (at: time) \<comment>\<open>time when this event occurs\<close>
-    (event: \<open>'world \<Rightarrow> 'world\<close>) \<comment>\<open>event: what actually happens\<close>
+    (at: time) \<comment>\<open>at: time when this event occurs\<close>
+    (event: \<open>'event\<close>) \<comment>\<open>event: what actually happens\<close>
   |
   RepeatingEvent
-    (at: time) \<comment>\<open>time when this event occurs\<close>
-    time \<comment>\<open>once the event completed, when this event is to be scheduled again.\<close>
-    (event: \<open>'world \<Rightarrow> 'world\<close>) \<comment>\<open>event: what actually happens\<close>
+    (at: time) \<comment>\<open>at: time when this event occurs\<close>
+    time \<comment>\<open>every: once the event completed, when this event is to be scheduled again.\<close>
+    (event: \<open>'event\<close>) \<comment>\<open>event: what actually happens\<close>
 
-fun next_events :: \<open>'world event \<Rightarrow> 'world event list\<close> where
+fun next_events :: \<open>'world timed_event \<Rightarrow> 'world timed_event list\<close> where
   "next_events (SingletonEvent _ _) = []"
 | "next_events (RepeatingEvent at_time every ev) = [RepeatingEvent (at_time + every) every ev]"
 
-type_synonym 'world future_event_list = \<open>'world event list\<close>
-
-datatype 'world discrete_event_simulator =
-  DiscreteEventSimulator
-    time    \<comment>\<open>now\<close>
-    'world  \<comment>\<open>current state\<close>
-    \<open>'world future_event_list\<close> \<comment>\<open>pending events\<close>
 
 (*sort may not be stable. There is a stable_sort_key*)
 beispiel
   \<open>sort_key at
-    [(RepeatingEvent 8 0 (id::unit\<Rightarrow>unit)), RepeatingEvent 4 0 id,
-     SingletonEvent 1 id, RepeatingEvent 42 0 id, RepeatingEvent 2 0 id]
-  = [SingletonEvent 1 id, RepeatingEvent 2 0 id,
-     RepeatingEvent 4 0 id, RepeatingEvent 8 0 id, RepeatingEvent 42 0 id]\<close>
+    [(RepeatingEvent 8 0 ''nothing''), RepeatingEvent 4 0 ''nothing'',
+     SingletonEvent 1 ''nothing'', RepeatingEvent 42 0 ''nothing'', RepeatingEvent 2 0 ''nothing'']
+  = [SingletonEvent 1 ''nothing'', RepeatingEvent 2 0 ''nothing'',
+     RepeatingEvent 4 0 ''nothing'', RepeatingEvent 8 0 ''nothing'', RepeatingEvent 42 0 ''nothing'']\<close>
   by simp
 
+type_synonym 'world future_event_list = \<open>'world timed_event list\<close>
+
+datatype ('world, 'event) discrete_event_simulator =
+  DiscreteEventSimulator
+    time    \<comment>\<open>now\<close>
+    'world  \<comment>\<open>current state\<close>
+    \<open>'event future_event_list\<close> \<comment>\<open>pending events\<close>
+
+
 (*we assume processing an event takes 0 time.*)
-definition duration :: "'world event \<Rightarrow> time" where
+definition duration :: "'event timed_event \<Rightarrow> time" where
   "duration _ \<equiv> 0"
 
-fun process_one :: "'world discrete_event_simulator \<Rightarrow> 'world discrete_event_simulator" where
-  "process_one (DiscreteEventSimulator now current fel) = 
+fun process_one ::
+  "('world, 'event) executor \<Rightarrow>
+   ('world, 'event) discrete_event_simulator \<Rightarrow> ('world, 'event) discrete_event_simulator"
+where
+  "process_one f (DiscreteEventSimulator now current fel) = 
     (case sort_key at fel
      of [] \<Rightarrow> DiscreteEventSimulator now current []
       | e#events \<Rightarrow>
          DiscreteEventSimulator
             ((max now (at e)) + (duration e))
-            ((event e) current)
+            (f current (event e))
             (events @ next_events e)
       )"
 
-beispiel \<open>process_one
+beispiel \<open>process_one (\<lambda>w e. if e = ''add42'' then w+42 else w)
   (DiscreteEventSimulator 0 (0::int)
-    [RepeatingEvent 8 0 id, RepeatingEvent 4 0 id, SingletonEvent 2 (\<lambda>i. i+42),
-     RepeatingEvent 42 0 id, RepeatingEvent 3 0 id]) =
+    [RepeatingEvent 8 0 ''nothing'',
+     RepeatingEvent 4 0 ''nothing'',
+     SingletonEvent 2 ''add42'',
+     RepeatingEvent 42 0 ''nothing'',
+     RepeatingEvent 3 0 ''nothing'']) =
   (DiscreteEventSimulator 2 42
-     [RepeatingEvent 3 0 id, RepeatingEvent 4 0 id, RepeatingEvent 8 0 id, RepeatingEvent 42 0 id])\<close>
+     [RepeatingEvent 3 0 ''nothing'',
+      RepeatingEvent 4 0 ''nothing'',
+      RepeatingEvent 8 0 ''nothing'',
+      RepeatingEvent 42 0 ''nothing''])\<close>
   by(simp add: duration_def)
 
-value \<open>(process_one^^5)
+text\<open>Execute 5 events:
+time 0
+time 0
+time 3
+time 6
+time 8\<close>
+beispiel \<open>((process_one (\<lambda>w _. w+1))^^5)
   (DiscreteEventSimulator 0 (0::int)
-    [RepeatingEvent 0 3 (\<lambda>i. i+1), RepeatingEvent 0 8 (\<lambda>i. i+1)])\<close>
+    [RepeatingEvent 0 3 ''X'', RepeatingEvent 0 8 ''X''])
+=
+DiscreteEventSimulator 8 5 [RepeatingEvent 9 3 ''X'', RepeatingEvent 16 8 ''X'']\<close> by eval
 
 (*what if we still have backlog, i.e. past events.
 Does `max now (at e)` do the right thing, i.e. process past events but do not progress or set back time?*)
-lemma "process_one (DiscreteEventSimulator now current fel) = DiscreteEventSimulator now' current' fel' \<Longrightarrow>
+lemma time_only_moves_forward:
+  "process_one f (DiscreteEventSimulator now current fel) =
+    DiscreteEventSimulator now' current' fel' \<Longrightarrow>
   now \<le> now'"
   apply(simp)
   apply(cases "sort_key at fel")
